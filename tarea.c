7dae5992799843ef72bb6f9c1ef89ce57cefe072
Your Name <you@example.com>
2025-05-06 21:20:00 -0400

    Dejé el main q hiciste y el mostrarMenu

--- tarea.c
+++ tarea.c
@@ -6,62 +6,36 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define MAX_ARTIST 256
+#define MAX_ID 512
+#define MAX_ALBUM 256
+#define MAX_TRACK 512
+#define MAX_GENRE 256
+
+//  Definición del struct tipoCanción
+
 typedef struct {
-    char id[100];
-    char artists[100];
-    char album_name[100];
-    char track_name[100];
-    int tempo;
-    char track_genre[100];
-} Canciones;
+    char id[MAX_ID];
+    char artist[MAX_ARTIST];
+    char album_name[MAX_ALBUM];
+    char track_name[MAX_TRACK];
+    char track_genre[MAX_GENRE];
+    float tempo;
+} tipoCancion;
 
 void mostrarMenuPrincipal() {
     limpiarPantalla();
     puts("========================================");
-    puts("     Base de Datos de Spotifind");
+    puts("      Base de Datos de Spotifind");
     puts("========================================");
-    puts("1) Cargar Canciones");
-    puts("2) Buscar por Género");
-    puts("3) Buscar por Artista");
-    puts("4) Buscar por Tempo");
-    puts("5) Crear Lista de Reproducción");
-    puts("6) Agregar Canción a lista");
-    puts("7) Mostrar canciones de una lista");
-    puts("8) Salir");
-}
-
-void leer_canciones() {
-    FILE *archivo = fopen("data/song_dataset_.csv", "r");
-    if (archivo == NULL) {
-        perror("Error al abrir el archivo");
-        return;
-    }
-
-    char **campos;
-    campos = leer_linea_csv(archivo, ','); // Lee los encabezados del CSV
-
-    int k = 0;
-    while ((campos = leer_linea_csv(archivo, ',')) != NULL) {
-        k++;
-        if (k > 10) break; // Solo carga las primeras 10 canciones
-
-        printf("ID: %d\n", atoi(campos[0]));
-        printf("Título canción: %s\n", campos[4]);
-
-        List *artistas = split_string(campos[2], ";");
-        printf("Artistas:\n");
-        for (char *artista = list_first(artistas); artista != NULL;
-             artista = list_next(artistas)) {
-            printf("  %s\n", artista);
-        }
-
-        printf("Álbum: %s\n", campos[3]);
-        printf("Género: %s\n", campos[20]);
-        printf("Tempo: %.2f\n", atof(campos[18]));
-        printf(" -------------------------------\n");
-    }
-
-    fclose(archivo);
+    puts("(1)   Cargar Canciones.");
+    puts("(2)   Buscar por Género.");
+    puts("(3)   Buscar por Artista.");
+    puts("(4)   Buscar por Tempo.");
+    puts("(5)   Crear Lista de Reproducción.");
+    puts("(6)   Agregar Canción a lista.");
+    puts("(7)   Mostrar canciones de una lista.");
+    puts("(8)   Salir.");
 }
 
 int main() {
